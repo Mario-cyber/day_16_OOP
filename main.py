832c57ae74fe14200146e1f98fa4ca4191147a44
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-03-02 22:12:08 +0000

    example table made

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from  prettytable import PrettyTable
 table = PrettyTable()
 
 
+table.add_column("Pokemon Name",["Pikachu","Charizard","Lapras"]) 
+table.add_column("Type",["Electric","Fire","Water"]) 
 
 print(table)
