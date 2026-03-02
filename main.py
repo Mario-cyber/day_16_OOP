3c46cc561c5f6955ed7825d5b008a8d8b1730e87
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-03-02 22:24:21 +0000

    comments added

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from  prettytable import PrettyTable
 
 table = PrettyTable()
 
-
+# '()' implies method
 table.add_column("Pokemon Name",["Pikachu","Charizard","Lapras"]) 
 table.add_column("Type",["Electric","Fire","Water"]) 
 
+# setting value by '=' implies attribute
 table.align = "l"
 print(table)
